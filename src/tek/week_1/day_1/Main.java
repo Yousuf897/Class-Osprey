0f15e348ee5d714b5c328182e9d01c6ad57d6282
Yousuf <124392065+Yousuf897@users.noreply.github.com>
2023-11-13 22:34:18 -0500

    Add files via upload

---
+++ src/tek/week_1/day_1/Main.java
@@ -0,0 +1,17 @@
+package tek.week_1.day_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println( "Welcome to the world of programming!" );
+
+        /*Welcome to the world of programming!*/
+
+        System.out.println( "I DON'T LIKE YOU COMPUTER!" );
+
+        /* I DON'T LIKE YOU COMPUTER! */
+
+    }
+
+}
