409a081e48feaf5c7e4a3092fac0fd6524c0eeef
Yousuf <124392065+Yousuf897@users.noreply.github.com>
2023-11-15 22:10:25 -0500

    Add files via upload

---
+++ src/week_1/day_3/TestVariable.java
@@ -0,0 +1,39 @@
+package tek.week_1.day_3;
+
+public class TestVariable {
+
+    public static void main(String[] args) {
+
+        double totalAmount; // --> Declaration
+        totalAmount = 2.6; // --> Initialization --> Assigning the value for the first time.
+        totalAmount = 3.6; // --> modification
+
+        // -----------------------------------------------------------------------------------------------
+
+        char grade = 'A';
+
+        System.out.println( " THIS IS MY JAVA PROGRAM! " );
+
+        System.out.println( grade );
+        System.out.println( totalAmount );
+
+        int number = 10;
+
+        char gender = 'M';
+
+        System.out.println(gender);
+
+        number = 50;
+
+        System.out.println(number);
+
+        // Declaration
+        double sumOfTwoNumbers;
+
+        gender = 'F';
+
+        System.out.println(gender);
+
+    }
+
+}
---
+++ src/week_1/day_3/WhatIsVariable.java
@@ -0,0 +1,17 @@
+package tek.week_1.day_3;
+
+public class WhatIsVariable {
+
+/*
+    Variables in Java are like containers or boxes where you can store information. Imagine you're moving to
+    a new house. You'd use boxes to pack different types of items: books, dishes, clothes, etc.
+    You might even label these boxes so that you know what's inside each one without having to open it up.
+
+    In Java, each variable is like one of those labeled boxes.
+
+*/
+
+
+
+
+}
